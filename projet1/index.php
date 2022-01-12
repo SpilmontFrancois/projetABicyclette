389665429d53b0eeaf615f6a1d21c868cd39fc72
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2022-01-12 15:39:12 +0100

    move files & add markers on map

--- projet1/index.php
+++ projet1/index.php
@@ -22,32 +22,67 @@ if ($http_response_header[0] === 'HTTP/1.1 200 OK') {
         echo $proc->transformToXml($data);
 
         $ipVeloStationURI = "http://www.velostanlib.fr/service/carto";
-        $dataVeloStation = simplexml_load_string(file_get_contents($ipVeloStationURI));
-        // todo: afficher les stations de vélos sur la map
+        $dataVeloStation = simplexml_load_string(file_get_contents($ipVeloStationURI))->markers;
+        $stations = array();
+        foreach ($dataVeloStation->marker as $station) {
+            $stations[] = array(
+                'number' => $station['number'],
+                'lat' => $station['lat'],
+                'lng' => $station['lng'],
+                'name' => $station['name'],
+                'address' => $station['address'],
+            );
+        }
 
-        echo "<br>";
-        // Affichage de la carte
-        $html = <<<HTML
-            <div id="map">
-            </div>
-            <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></script>
-            <script>
-                function initMap() {
-                    myMap = L.map('map').setView([$geolocData->lat, $geolocData->lon], 15)
-                    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-                    // Lien vers la source des données
-                    attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
-                    minZoom: 14,
-                    maxZoom: 17
-                    }).addTo(myMap)
-                    L.marker([$geolocData->lat, $geolocData->lon]).addTo(myMap)
-                }
-                window.onload = function () {
-                    initMap()
-                }
-            </script>
-            HTML;
-        echo $html;
+        $urlPlaces = "http://www.velostanlib.fr/service/stationdetails/nancy/";
+        $newTab = array();
+        foreach ($stations as $station) {
+            $data = simplexml_load_string(file_get_contents($urlPlaces . $station['number']));
+            $newTab[] = array(
+                'number' => $station['number'],
+                'lat' => $station['lat'],
+                'lng' => $station['lng'],
+                'name' => $station['name'],
+                'address' => $station['address'],
+                'bikes' => $data->available,
+                'slots' => $data->free
+            );
+        }
+        $json = json_encode($newTab);
+
+        if ($http_response_header[0] === 'HTTP/1.1 200 OK') {
+            echo "<br>";
+            // Affichage de la carte
+            $html = <<<HTML
+                <div id="map">
+                </div>
+                <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></script>
+                <script>
+                    function initMap() {
+                        myMap = L.map('map').setView([$geolocData->lat, $geolocData->lon], 15)
+                        L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+                            // Lien vers la source des données
+                            attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+                            minZoom: 14,
+                            maxZoom: 17
+                        }).addTo(myMap)
+                        L.marker([$geolocData->lat, $geolocData->lon]).addTo(myMap)
+                        // Par magie, ça marche SEULEMENT si on a ce console.log donc pas touche
+                        console.log($json);
+                        $json.forEach((el)=>{
+                           L.marker([el.lat[0], el.lng[0]]).addTo(myMap)
+                           .bindPopup(
+                                 "<b>" + el.name[0] + "</b><br>" + el.address[0] + "<br>" + el.bikes[0] + " vélos disponibles<br>" + el.slots[0] + " places disponibles"
+                           )
+                        })
+                    }
+                    window.onload = function () {
+                        initMap()
+                    }
+                </script>
+                HTML;
+            echo $html;
+        }
     } else {
         echo "Erreur lors de la récupération des données météo";
     }
